f61005adf88bab798458b4742a0bb1bf1a7e4fb1
yamadahideto <yamadahideto@yamadahidetonoMacBook-Pro.local>
2019-03-29 13:41:46 +0900

    Edit migrationFile user_groups

--- db/migrate/20190329031517_create_user_groups.rb
+++ db/migrate/20190329031517_create_user_groups.rb
@@ -1,8 +1,12 @@
 class CreateUserGroups < ActiveRecord::Migration[5.0]
   def change
     create_table :user_groups do |t|
-
+      t.string		 :user_id, null: false, foreign_key: true
+      t.string		 :group_id, null: false, foreign_key: true
       t.timestamps
+
+	belongs_to :group
+	belongs_to :user
     end
   end
 end
