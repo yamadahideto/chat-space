8b5d16bad6cc036b9c505c80aac0bd57780687cc
yamadahideto <yamadahideto@yamadahidetonoMacBook-Pro.local>
2019-03-29 13:00:49 +0900

    Edit migrationFile users

--- db/migrate/20190327073609_create_users.rb
+++ db/migrate/20190327073609_create_users.rb
@@ -1,8 +1,13 @@
 class CreateUsers < ActiveRecord::Migration[5.0]
   def change
     create_table :users do |t|
-
+      t.string			:name, index: true, null: false, unique: true
+      t.string			:mail, null: false
       t.timestamps
+
+	has_many :groups, through: user_groups
+	has_many :messages
+	has_many :user_groups
     end
   end
 end
