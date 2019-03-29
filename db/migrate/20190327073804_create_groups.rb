94541977f460a83761d5b17104f1747c97a38252
yamadahideto <yamadahideto@yamadahidetonoMacBook-Pro.local>
2019-03-29 13:35:26 +0900

    Edit migrationFile groups

--- db/migrate/20190327073804_create_groups.rb
+++ db/migrate/20190327073804_create_groups.rb
@@ -1,8 +1,12 @@
 class CreateGroups < ActiveRecord::Migration[5.0]
   def change
     create_table :groups do |t|
-
+      t.string 		:name, null: false
       t.timestamps
-    end
+
+	 has_many: users, through: user_groups
+	 has_many: messages
+	 has_many: user_groups
+	 end
   end
 end
