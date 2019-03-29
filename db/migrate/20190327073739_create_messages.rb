2931a1cffb256db5fcde84fba98afb752509448d
yamadahideto <yamadahideto@yamadahidetonoMacBook-Pro.local>
2019-03-29 13:33:10 +0900

    Edit migrationFile messages

--- db/migrate/20190327073739_create_messages.rb
+++ db/migrate/20190327073739_create_messages.rb
@@ -1,8 +1,14 @@
 class CreateMessages < ActiveRecord::Migration[5.0]
   def change
     create_table :messages do |t|
-
+      t.text		:body
+      t.string      :image
+      t.integer     :user_id, null: false, foreign_key: true
+      t.integer     :group_id, null: false, foreign_key: true
       t.timestamps
+
+      belongs_to :user
+	  belongs_to :group
     end
   end
 end
