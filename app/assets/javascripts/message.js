4cdedf90a9a2f0e8fbcea0693ce44065cb34b0a9
yamadahideto <yamadahideto@yamadahidetonoMacBook-Pro.local>
2019-04-11 10:38:37 +0900

    Edit asynchronous

---
+++ app/assets/javascripts/message.js
@@ -0,0 +1,57 @@
+
+$(function(){
+	function buildHTML(message){
+
+		var msg = ``
+      if ( message.content != null){
+      	msg = `<p class="chat-main-messages-box__text">
+							${message.content}
+						</p>`
+      }
+
+		var img = ``
+	    if(message.image != null){
+	    	 img = `<img class="lower-message__image" src="${message.image}">`
+					}
+
+
+
+var html =
+		`<div class="chat-main-messages-box">
+			<div class="hat-main-messages-box__pper-info">
+				${message.user_name}
+			</div>
+			<div class="chat-main-messages-box__date">
+				${message.created_at}
+			</div>
+		</div>
+		<div class="lower-message">`
+			+ msg + img +
+		`</div>`;
+	 return html
+	}
+
+	$('#new_message').on('submit', function(e){
+		e.preventDefault();
+		var formData = new FormData(this);
+		$.ajax({
+			url:location.href,
+			type: "POST",
+			data: formData,
+			dataType: 'json',
+			processData: false,
+			contentType: false
+		})
+		.done(function(data){
+			var html = buildHTML(data);
+			$('.chat-main-messages').append(html);
+			$('.form').val('');
+			$('.btn').prop('disabled', false);
+			$('.chat-main-messages').animate({scrollTop: 100000});
+
+		})
+		.fail(function(){
+			alert('error')
+		})
+	})
+});
