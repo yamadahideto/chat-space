6f60f22d9f6ecd71e1278beb22c1d071b95df2fb
yamadahideto <yamadahideto@yamadahidetonoMacBook-Pro.local>
2019-04-20 19:13:42 +0900

    Commit

--- config/deploy.rb
+++ config/deploy.rb
@@ -10,7 +10,7 @@ set :rbenv_type, :user
 set :rbenv_ruby, '2.3.1'
 
 set :ssh_options, auth_methods: ['publickey'],
-                  keys: ['~/.ssh/yadmt-rf.pem.pem']  ※例：~/.ssh/key_pem.pem
+                  keys: ['~/.ssh/yadmt-rf.pem.pem']
 
 set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
 set :unicorn_config_path, -> { "#{current_path}/config/unicorn.rb" }
