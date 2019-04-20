ad036799f543c94474146c6d22c5c85e729e2d9a
yamadahideto <yamadahideto@yamadahidetonoMacBook-Pro.local>
2019-04-20 19:30:58 +0900

    Commit

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -49,9 +49,7 @@
 #
 # The server-based syntax can be used to override options:
 # ------------------------------------
-# server "example.com",
-#   user: "user_name",
-#   roles: %w{web app},
+server '52.69.89.34', user: 'ec2-user', roles: %w{app db web}
 #   ssh_options: {
 #     user: "user_name", # overrides user setting above
 #     keys: %w(/home/user_name/.ssh/id_rsa),
@@ -59,4 +57,3 @@
 #     auth_methods: %w(publickey password)
 #     # password: "please use keys"
 #   }
-server '52.69.89.34', user: 'ec2-user', roles: %w{app db web}
